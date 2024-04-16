80cc2749f2407e3d7324a5dc1b74145a8e921059
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 08:27:58 +0900

    step9 토끼 잡기 게임 생성 완료

---
+++ src/main/java/step9/Rabbit.java
@@ -0,0 +1,23 @@
+package step9;
+
+import java.util.Random;
+
+public class Rabbit {
+    private static final Random RANDOM = new Random();
+
+    private static final int MOVE_RANGE = 10;
+
+    private int rabbitPosition = 0;
+
+    public int move() {
+        return rabbitPosition += RANDOM.nextInt(MOVE_RANGE * 2 + 1) - MOVE_RANGE;
+    }
+
+    public int position() {
+        return rabbitPosition;
+    }
+
+    public int resetPosition(){
+        return rabbitPosition = 0;
+    }
+}
