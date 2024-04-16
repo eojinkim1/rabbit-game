db88a6c0741fb4373cfe6e91f3edd7aa084c8211
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 07:33:58 +0900

    step7 Restart 클래스 코드 생성 완료

---
+++ src/main/java/step7/GameRestart.java
@@ -0,0 +1,22 @@
+package step7;
+
+import java.util.Scanner;
+
+public class GameRestart {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static boolean askRestart() {
+        while (true) {
+            System.out.println("게임을 다시 시작하시겠습니까? ( Y / N )");
+            String restart = SCANNER.next().toUpperCase();
+            if (restart.equals("Y")) {
+                System.out.println("새로운 게임을 시작합니다.");
+                return true;
+            } else if (restart.equals("N")){
+                System.out.println("게임을 종료합니다.");
+                return false;
+            }
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+}
