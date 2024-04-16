d94ff01cee0c6eca2ce5382189c96af2f753fcfa
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 05:55:14 +0900

    step5 Player 클래스 생성 완료

---
+++ src/main/java/step5/Player.java
@@ -0,0 +1,15 @@
+package step5;
+
+import java.util.Scanner;
+
+public class Player {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private int playerPosition = 0;
+    public int move(){
+        return playerPosition += SCANNER.nextInt();
+    }
+    public boolean rabbitCatch(Rabbit rabbit){
+        return playerPosition == rabbit.position();
+    }
+}
