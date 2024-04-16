316760ed5606f9ccf44a0f0a5d37766aeb1b08f6
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 07:34:06 +0900

    step7 Player 클래스 코드 생성 완료

---
+++ src/main/java/step7/Player.java
@@ -0,0 +1,18 @@
+package step7;
+
+import java.util.Scanner;
+
+public class Player {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private int playerPosition = 0;
+    public int move(){
+        return playerPosition += SCANNER.nextInt();
+    }
+    public boolean rabbitCatch(Rabbit rabbit){
+        return playerPosition == rabbit.position();
+    }
+    public int resetPlayerPosition(){
+        return playerPosition = 0;
+    }
+}
