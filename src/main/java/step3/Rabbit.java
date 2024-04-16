35024ef85ec5a89cacaabee061ff3cede4987c28
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 05:14:57 +0900

    step3 Rabbit 클래스 생성 완료

---
+++ src/main/java/step3/Rabbit.java
@@ -0,0 +1,16 @@
+package step3;
+
+import java.util.Random;
+
+public class Rabbit {
+    private static final Random RANDOM = new Random();
+    private static final int MOVE_RANGE = 10;
+    private int locationX = 0;
+
+    public int move() {
+    return locationX += RANDOM.nextInt(MOVE_RANGE * 2 + 1) - MOVE_RANGE;
+    }
+    public int position(){
+        return locationX;
+    }
+}
