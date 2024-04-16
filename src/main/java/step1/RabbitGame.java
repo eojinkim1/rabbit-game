a89c566c071f77e0f67b01e78f89f589b48fcdec
eojinkim1 <rladjwls999@gmail.com>
2024-04-16 14:56:02 +0900

    코드 수정

--- src/main/java/step1/RabbitGame.java
+++ src/main/java/step1/RabbitGame.java
@@ -10,19 +10,18 @@ public class RabbitGame {
 
     private static int RESULT = 1;
     public static void main(String[] args) {
-        printGameRules();
+//        printGameRules();
 
         System.out.println("토끼잡기 게임 시작합니다!");
 
         while (true) {
+            int rabbitLocation = getRandomLocation();
             try {
-                int rabbitLocation = getRandomLocation();
-//                System.out.println("토끼가 이동합니다.");
                 System.out.println("1~10 사이의 숫자를 입력하여 토끼가 있는 칸을 예측해주세요!");
-//                System.out.println("토끼가 있는 곳 : " + rabbitLocation);
-                int user = SCANNER.nextInt();
+                System.out.println("토끼가 있는 곳 : " + rabbitLocation);
+                int userInput = getUserInput();
 
-                if (user == rabbitLocation) {
+                if (userInput == rabbitLocation) {
                     System.out.println("토끼를 잡았습니다.");
                 } else {
                     System.out.println("토끼가 이동했습니다.");
@@ -35,41 +34,37 @@ public class RabbitGame {
                 continue;
             }
             System.out.println(RESULT + "번 만에 토끼를 잡았습니다.");
-            while (true) {
-                System.out.println("게임을 다시 시작하시겠습니까? ( Y / N )");
-                String restart = SCANNER.next().toUpperCase();
-                if (restart.equals("Y")) {
-                    System.out.println("새로운 게임을 시작합니다.");
-                    break;
-                } else if (restart.equals("N")) {
-                    System.out.println("게임을 종료합니다.");
-                    return;
-                } else {
-                    System.out.println("잘못된 입력입니다.");
-                }
+            if (!GameRestart.askRestart()){
+                return;
+            }else {
+                RESULT = 1;
             }
         }
+    }
+
+    private static int getUserInput() {
+        return SCANNER.nextInt();
     }
 
     private static int getRandomLocation() {
         return RANDOM.nextInt(10) + 1;
     }
 
-    private static void printGameRules() {
-        System.out.println("===========================================");
-        System.out.println("토끼잡기 게임 룰 설명");
-        System.out.println("===========================================");
-
-        System.out.println("*게임 목표*");
-        System.out.println("10칸 중에 숨어있는 토끼를 찾아 잡는 게임입니다.");
-
-        System.out.println("*게임 진행*");
-        System.out.println("1. 컴퓨터가 토끼가 숨어있는 칸을 랜덤으로 선택합니다.");
-        System.out.println("2. 플레이어는 1~10 사이의 숫자를 입력하여 토끼가 있는 칸을 추측합니다.");
-        System.out.println("3. 플레이어가 입력한 숫자가 토끼가 있는 칸과 일치하면 토끼를 잡고 게임 종료됩니다.");
-        System.out.println("4. 플레이어가 입력한 숫자가 토끼가 있는 칸과 일치하지 않으면 토끼가 이동하고 플레이어는 다시 숫자를 입력할 기회를 얻습니다.");
-
-        System.out.println("*게임 종료*");
-        System.out.println("토끼를 잡으면 게임이 종료됩니다.");
-    }
+//    private static void printGameRules() {
+//        System.out.println("===========================================");
+//        System.out.println("토끼잡기 게임 룰 설명");
+//        System.out.println("===========================================");
+//
+//        System.out.println("*게임 목표*");
+//        System.out.println("10칸 중에 숨어있는 토끼를 찾아 잡는 게임입니다.");
+//
+//        System.out.println("*게임 진행*");
+//        System.out.println("1. 컴퓨터가 토끼가 숨어있는 칸을 랜덤으로 선택합니다.");
+//        System.out.println("2. 플레이어는 1~10 사이의 숫자를 입력하여 토끼가 있는 칸을 추측합니다.");
+//        System.out.println("3. 플레이어가 입력한 숫자가 토끼가 있는 칸과 일치하면 토끼를 잡고 게임 종료됩니다.");
+//        System.out.println("4. 플레이어가 입력한 숫자가 토끼가 있는 칸과 일치하지 않으면 토끼가 이동하고 플레이어는 다시 숫자를 입력할 기회를 얻습니다.");
+//
+//        System.out.println("*게임 종료*");
+//        System.out.println("토끼를 잡으면 게임이 종료됩니다.");
+//    }
 }
