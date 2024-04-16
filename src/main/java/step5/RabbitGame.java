12d3c7b74d6d35255fc3af60af179738e14d8505
eojinkim1 <rladjwls999@gmail.com>
2024-04-17 05:55:48 +0900

    step5 토끼 잡기 게임 생성 완료

---
+++ src/main/java/step5/RabbitGame.java
@@ -0,0 +1,31 @@
+package step5;
+
+import java.util.InputMismatchException;
+
+public class RabbitGame {
+    public static void main(String[] args) {
+        System.out.println("토끼 잡기 게임 시작입니다.");
+        Player player = new Player();
+        Rabbit rabbit = new Rabbit();
+
+        while (true) {
+            System.out.println("-10 ~ 10 사이의 숫자를 입력하여 토끼가 있는 칸을 예측해주세요!");
+            try {
+                System.out.println("내 위치 : " + player.move());
+                System.out.println("토끼 위치 : " + rabbit.move());
+                boolean isCatch = player.rabbitCatch(rabbit);
+                if (isCatch) {
+                    System.out.println("토끼를 잡았습니다.");
+                    return;
+                }
+                System.out.println("토끼가 이동했습니다.");
+                continue;
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다.");
+            }
+            if (!GameRestart.askRestart()) {
+                return;
+            }
+        }
+    }
+}
