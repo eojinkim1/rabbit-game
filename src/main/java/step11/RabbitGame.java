05b4e750c7467a44735dd0b0e66796a95f04b553
eojinkim1 <rladjwls999@gmail.com>
2024-04-18 06:07:44 +0900

    step11 토끼 잡기 게임 생성 완료

---
+++ src/main/java/step11/RabbitGame.java
@@ -0,0 +1,9 @@
+package step11;
+
+public class RabbitGame {
+    public static void main(String[] args) {
+        GameLoop gameLoop = new GameLoop();
+        System.out.println("토끼 잡기 게임 시작입니다.");
+        gameLoop.run();
+    }
+}
