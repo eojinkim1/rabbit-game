3b2123e095d29a87b7001a53c7ccb3363c7f56ab
eojinkim1 <rladjwls999@gmail.com>
2024-04-18 05:08:38 +0900

    step10 GameStart 클래스 생성 완료

---
+++ src/main/java/step10/GameStart.java
@@ -0,0 +1,29 @@
+package step10;
+
+import java.util.InputMismatchException;
+
+public class GameStart {
+       private Player player = new Player();
+        private Rabbit rabbit = new Rabbit();
+    public void game() {
+
+        while (true) {
+            try {
+                System.out.println("내 위치 : " + player.move());
+                System.out.println("토끼 위치 : " + rabbit.move());
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.println("올바른 형식의 입력이 아닙니다. 다시 입력해주세요.");
+                continue;
+            }
+            boolean isCatch = player.rabbitCatch(rabbit);
+            if (isCatch) {
+                System.out.println("토끼를 잡았습니다.");
+                player = new Player();
+                rabbit = new Rabbit();
+                return ;
+            } else {
+                System.out.println("토끼가 이동합니다.");
+            }
+        }
+    }
+}
