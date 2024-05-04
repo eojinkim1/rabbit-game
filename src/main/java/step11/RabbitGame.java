28a12bcd488a3799592cd7af72d971402c927b0b
eojinkim1 <rladjwls999@gmail.com>
2024-05-04 21:27:58 +0900

    게임 시작 새로운 출력문 추가

--- src/main/java/step11/RabbitGame.java
+++ src/main/java/step11/RabbitGame.java
@@ -4,6 +4,7 @@ public class RabbitGame {
     public static void main(String[] args) {
         GameLoop gameLoop = new GameLoop();
         System.out.println("토끼 잡기 게임 시작입니다.");
+        System.out.println("-10 ~ 10 사이의 숫자를 입력하여 토끼가 있는 칸을 예측해주세요!");
         gameLoop.run();
     }
 }
